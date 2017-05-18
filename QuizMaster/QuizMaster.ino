8ca9063fbbbef378edc2e2b14fc5c8f11a2b9e3a
josss000 <josss@MacBook-Pro-de-Josss.local>
2017-05-18 11:33:13 +0200

    Include Quirino library

--- QuizMaster/QuizMaster.ino
+++ QuizMaster/QuizMaster.ino
@@ -3,62 +3,23 @@
 #include "nRF24L01.h"
 #include "RF24.h"
 #include "printf.h"
-
-/**** Type ****/
-
-typedef struct {
-  int gameMode;
-  int sender; // Who sends the message ? 0 = Master
-  int target; // Who is the message for ? 0 = Master
-  int info;
-} message;
-
-/**** Constants ****/
-
-// Config
-const int MAX_NB_ANSWERS = 5;       // Max number of answers stored for each question
+#include "Quizzino.h"
+#include "Quizzino_Master.h"
+#include "Quizzino_Common.h"
 
 // Wiring
-const int pinCE = 9;
-const int pinCSN = 10;
-
 const int pinLEDBlue = 2;
 const int pinLEDGreen = 3;
 const int pinButton = 4;
 
-// RF24 Config
-const int PAYLOAD_SIZE = 32;
-
-// Transmission Canals
-const byte CANAL_MASTER_TO_SLAVE[5] = "RFM2S";
-const byte CANAL_SLAVE_TO_MASTER[5] = "RFS2M";
-
-// Quiz modes
-const int MODE_REGISTER = 1;    // Buzzer waits for the master to recognize it
-const int MODE_PLAYING = 2;     // Buzzer is playing
-const int MODE_LOCKED = 3;      // Buzzer is locked : no answer is sent
-
-// Messages
-const int MSG_INIT = 0;               // Master waits for buzzer to register
-const int MSG_NEW_ID = 1;             // Master gives buzzer its id
-const int MSG_BUZZER_SIGNED_IN = 2;   // Master sends feedback : buzzer has been recognized
-const int MSG_GAME_ON = 3;            // Master waits for answer
-const int MSG_FIRST_ANSWER = 4;       // Master says answer is correct : leds are lit
-const int MSG_WRONG_ANSWER = 5;       // Master says answer is wrong : buzzer is locked
-const int MSG_REGISTER = 6;           // Buzzer wants to register
-const int MSG_ANSWERED = 7;           // Buzzer sends answeranswer
-
-// Targets
-const int TARGET_MASTER = 0;
-const int TARGET_ALL = -1;
 
 /**** Variables ****/
+
 int currentMode;  // Stores the game status
-RF24 radio(pinCE,pinCSN); // Set up nRF24L01 radio on SPI bus pins
+
 int buzzerCount;
 int answersList[MAX_NB_ANSWERS];
 int answersNb;
-message receivedMessage;
 
 //TEMP
 int timeLight;
@@ -67,10 +28,6 @@ int buttonState = HIGH;  // Last button state (default open)
 
 
 
-
-
-bool newData = false;
-
 //===========
 
 void setup() {
@@ -78,6 +35,8 @@ void setup() {
   // Open Serial Interface for debugging
   Serial.begin(9600);
 
+  myself = TARGET_MASTER;
+  
   initTransmission();
 
   //  const int currentMode = MODE_REGISTER; // On startup, the master waits for the buzzer to register
@@ -97,37 +56,6 @@ void loop() {
   if (radio.available()) readMessage();
 
 //  checkForUserInput();
-}
-
-void initTransmission()
-{
-  Serial.println("Quizino Master Starting");
-  
-  // Begin Radio Transmission
-  radio.begin();
-//  radio.setPayloadSize(PAYLOAD_SIZE); // We don't need to transmit a lot of data
-  radio.setDataRate(RF24_250KBPS);
-  radio.setRetries(3,5); // delay, count
-
-  // Open Emitting / Receiving transmission canals
-  // This is the Master code, so :
-  //   -  It writes on the Master -> Slave channel
-  //   -  It listens on the Slave -> Master channel
-  radio.openWritingPipe(CANAL_MASTER_TO_SLAVE);
-  radio.openReadingPipe(1,CANAL_SLAVE_TO_MASTER);
-
-  radio.startListening(); 
-}
-
-
-void turnLightOn(int pin)
-{
-  digitalWrite(pin, LOW);
-}
-
-void turnLightOff(int pin)
-{
-  digitalWrite(pin, HIGH);
 }
 
 void readMessage()
@@ -160,44 +88,13 @@ void readMessage()
   }
 }
 
-bool sendMessage(int gameMode, int target, int msg)
-{
-  Serial.println("---");
-  Serial.println("Envoi du message");
-  radio.stopListening();
-
-  message messageToSend;
-  messageToSend.sender = TARGET_MASTER;
-  messageToSend.target = target;
-  messageToSend.info = msg;
-  bool ok = radio.write( &messageToSend, sizeof(messageToSend) );
-  
-  radio.startListening();
-
-  Serial.print("Message From : ");
-  Serial.println(messageToSend.sender);
-  Serial.print("Info : ");
-  Serial.println(messageToSend.info);
-  Serial.print("Target : ");
-  Serial.println(messageToSend.target);
-  Serial.print("GameMode : ");
-  Serial.println(messageToSend.gameMode);
-  Serial.println("---");
-  Serial.print("Résultat : ");
-  if(ok) Serial.println("Envoyé");
-  else Serial.println("Raté");
-  Serial.println("---");
-
-}
-
-
 void storeAnswer(int buzzerId)
 {
   answersNb++;
   answersList[answersNb] = buzzerId;
       
   // only gives feedback for the first answer
-  if(answersNb == 1) sendMessage(MODE_PLAYING, buzzerId, MSG_FIRST_ANSWER);
+  if(answersNb == 1) sendMessage(buzzerId, MSG_FIRST_ANSWER);
 }
 
 
--- QuizSlave/QuizSlave.ino
+++ QuizSlave/QuizSlave.ino
@@ -4,89 +4,28 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-/**** Type ****/
-
-typedef struct {
-  int gameMode;
-  int sender; // Who sends the message ? 0 = Master
-  int target; // Who is the message for ? 0 = Master
-  int info;
-} message;
-
-/**** Constants ****/
-
-// Buzzer Id
-int buzzerId = 1;
+#include "printf.h"
+#include "Quizzino.h"
+#include "Quizzino_Common.h"
 
 // Wiring
-const int pinCE = 9;
-const int pinCSN = 10;
-
 const int pinLED = 2;
 const int pinButton = 4;
 
-// RF24 Config
-const int PAYLOAD_SIZE = 32;
-
-// Transmission Canals
-const byte CANAL_MASTER_TO_SLAVE[5] = "RFM2S";
-const byte CANAL_SLAVE_TO_MASTER[5] = "RFS2M";
-
-// Quiz modes
-const int MODE_REGISTER = 1;    // Buzzer waits for the master to recognize it
-const int MODE_PLAYING = 2;     // Buzzer is playing
-const int MODE_LOCKED = 3;      // Buzzer is locked : no answer is sent
-
-// Messages
-const int MSG_INIT = 0;               // Master waits for buzzer to register
-const int MSG_NEW_ID = 1;             // Master gives buzzer its id
-const int MSG_BUZZER_SIGNED_IN = 2;   // Master sends feedback : buzzer has been recognized
-const int MSG_GAME_ON = 3;            // Master waits for answer
-const int MSG_FIRST_ANSWER = 4;       // Master says answer is correct : leds are lit
-const int MSG_WRONG_ANSWER = 5;       // Master says answer is wrong : buzzer is locked
-const int MSG_REGISTER = 6;           // Buzzer wants to register
-const int MSG_ANSWERED = 7;           // Buzzer sends answer
-
-// Targets
-const int TARGET_MASTER = 0;
-const int TARGET_ALL = -1;
-
 /**** Variables ****/
-
-RF24 radio(pinCE,pinCSN); // Set up nRF24L01 radio on SPI bus pins
 
 int buzzerMode;  // Stores the game status
 int buttonState = HIGH;  // Last button state (default open)
-message sendMessage;
 
 //TEMP
 int timeLight;
 
-
-
-
-
-
-
-
-
-
-
-char dataToSend[10] = "Message 0";
-char txNum = '0';
-
-
-unsigned long currentMillis;
-unsigned long prevMillis;
-unsigned long txIntervalMillis = 1000; // send once per second
-
-
-
 void setup() {
 
   Serial.begin(9600);
-    
+
+  myself = 13;
+  
   initTransmission();
   initLedAndButton();
 
@@ -113,87 +52,24 @@ void loop() {
   {
     // Button has been pushed
     Serial.print("Réponse :");
-    Serial.println(buzzerId);
+    Serial.println(myself);
     Serial.println("");
-    sendAnswer();
-
+    sendMessage(TARGET_MASTER, MSG_ANSWERED);
+    
     timeLight = millis();   
   }
   
   buttonState = currentButtonState;
 
   // TEMP
-  if (millis() - timeLight > 200)  turnLightOff();
+  if (millis() - timeLight > 200)  turnLightOff(pinLED);
 }
-
-void initTransmission()
-{
-  Serial.println("Quizino Slave Starting");
-  
-  // Begin Radio Transmission
-  radio.begin();
-//  radio.setPayloadSize(PAYLOAD_SIZE); // We don't need to transmit a lot of data
-  radio.setDataRate( RF24_250KBPS );
-  radio.setRetries(3,5); // delay, count
-  
-  // Open Emitting / Receiving transmission canals
-  // This is the Master code, so :
-  //   -  It writes on the Master -> Slave channel
-  //   -  It listens on the Slave -> Master channel
-  radio.openWritingPipe(CANAL_SLAVE_TO_MASTER);
-  radio.openReadingPipe(1,CANAL_MASTER_TO_SLAVE);
-
-  radio.startListening(); 
-}
-
-//====================
-
-
-void sendAnswer()
-{
-  radio.stopListening();
-
-  message messageToSend;
-  messageToSend.sender = buzzerId;
-  messageToSend.target = TARGET_MASTER;
-  messageToSend.info = MSG_ANSWERED;
-  bool ok = radio.write( &messageToSend, sizeof(messageToSend) );
-
-  radio.startListening();
-
-  Serial.print("Message From : ");
-  Serial.println(messageToSend.sender);
-  Serial.print("Info : ");
-  Serial.println(messageToSend.info);
-  Serial.print("Target : ");
-  Serial.println(messageToSend.target);
-  Serial.print("GameMode : ");
-  Serial.println(messageToSend.gameMode);
-  Serial.println("---");
-  Serial.print("Résultat : ");
-  if(ok) Serial.println("Envoyé");
-  else Serial.println("Raté");
-  Serial.println("---");
-
-}
-
-//================
 
 void initLedAndButton()
 {
     pinMode(pinLED, OUTPUT);
     pinMode(pinButton, INPUT);
-    turnLightOff();
-}
-
-void turnLightOn()
-{
-  digitalWrite(pinLED, LOW);
-}
-
-void turnLightOff()
-{
-  digitalWrite(pinLED, HIGH);  
+    turnLightOff(pinLED);
 }
 
 void readMessage()
@@ -213,11 +89,11 @@ void readMessage()
   } // Message is not for me
   else Serial.println("C'est pour moi");
     
-  switch (receivedMessage.gameMode)
+  switch (receivedMessage.info)
   {
     case MSG_NEW_ID:
     // buzzerReceives its Id
-      buzzerId = receivedMessage.info;
+      myself = receivedMessage.info;
       break;
       
     case MSG_BUZZER_SIGNED_IN:
@@ -226,13 +102,13 @@ void readMessage()
       
     case MSG_GAME_ON:
       // Sets Playing mode : if the user pushes the buzzer, a message is sent to the master
-      turnLightOff();
+      turnLightOff(pinLED);
       buzzerMode = MODE_PLAYING;
       break;
       
     case MSG_FIRST_ANSWER:
       // The buzzer has been pushed first. Lights are on
-      turnLightOn();
+      turnLightOn(pinLED);
       break;
       
     case MSG_WRONG_ANSWER:
